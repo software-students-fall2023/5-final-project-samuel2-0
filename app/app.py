42c03cb8689d9dcc90aea29bd9ea2bfd319fea5f
Lefie <qiwenruan11@gmail.com>
2023-12-13 18:21:07 -0500

    signup good

--- app/app.py
+++ app/app.py
@@ -1,40 +1,73 @@
 
 
-from flask import Flask,render_template
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    url_for,
+    request
+    )
 from pymongo import MongoClient
+import bson
+
 
 app = Flask(__name__)
 
-"""
+
+#"mongodb://mongo:27017/"
 def connection_db():
-  
-    return MongoClient("mongodb://mongo:27017/")
+    """
+    Connect to DB
+    """
+    return MongoClient("mongodb://localhost:27017/")
 
 client = connection_db()
-db = client["db"]
+db = client["letterbox_db"]
 
-db.users.insert_one({"name":"lemon"})
-"""
+#db.users.insert_one({"name":"lemon"})
 
-@app.route("/")
+
+@app.route("/", methods=["GET"])
 def welcome():
     return render_template("index.html")
-
-"""
-@app.route("/test")
-def test():
-    user = db.users.find_one({"name":"lemon"})
-    return render_template("test.html", user=user)
-"""
 
 
 @app.route("/login")
 def login():
     return render_template("login.html")
 
-@app.route("/signup")
+
+
+@app.route("/signup", methods=["POST", "GET"])
 def signup():
-    return render_template("signup.html")
+    
+    if request.method == 'POST':
+
+        username = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        #check if user already exists, if so redirect to log in
+        user = db.users.find_one({"name": username})
+        if(user is not None):
+            print("user already exists")
+            return redirect(url_for("login"))
+
+        #if user is None, create a new account
+        account = {"email": email,
+                "name": username,
+                "password": password,
+                }
+        
+        db.users.insert_one(account)
+        
+        
+    else:
+        return render_template("signup.html")
+    
+
+
+    return redirect(url_for("welcome"))
 
 @app.route("/inbox")
 def inbox():
